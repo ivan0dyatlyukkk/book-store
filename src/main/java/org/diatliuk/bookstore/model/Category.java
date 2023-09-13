b11107a698f9e71645b763f6e1087d341f90fca3
Ivan Diatliuk <dyatlyukk.ivan@gmail.com>
2023-09-13 20:31:09 +0300

    added category entity and an appropriate liquibase file for creating new table

---
+++ src/main/java/org/diatliuk/bookstore/model/Category.java
@@ -0,0 +1,18 @@
+package org.diatliuk.bookstore.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+}
