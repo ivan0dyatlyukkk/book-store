6951eb480c3eb948ff21698233eaaa8de61fd939
Ivan Diatliuk <dyatlyukk.ivan@gmail.com>
2023-09-18 19:44:56 +0300

    corrected mistakes related to users-roles and books-categories tables and removed notNull annotations in repository

--- src/main/java/org/diatliuk/bookstore/repository/book/BookRepository.java
+++ src/main/java/org/diatliuk/bookstore/repository/book/BookRepository.java
@@ -1,6 +1,5 @@
 package org.diatliuk.bookstore.repository.book;
 
-import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Optional;
 import org.diatliuk.bookstore.model.Book;
@@ -8,23 +7,19 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
-public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findAllByCategoriesId(Long categoryId);
 
-    @NotNull
     @Override
     @Query("FROM Book b LEFT JOIN FETCH b.categories WHERE b.id = ?1")
-    Optional<Book> findById(@NotNull Long id);
+    Optional<Book> findById(Long id);
 
-    @NotNull
     @Override
     @Query("FROM Book b LEFT JOIN FETCH b.categories")
-    Page<Book> findAll(@NotNull Pageable pageable);
+    Page<Book> findAll(Pageable pageable);
 
-    @NotNull
     @Query("FROM Book b LEFT JOIN FETCH b.categories")
-    List<Book> findAll(@NotNull Specification<Book> specification);
+    List<Book> findAll(Specification<Book> specification);
 }
