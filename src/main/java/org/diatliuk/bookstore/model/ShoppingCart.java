6436770151f052523c7a5121914841334b8c48f9
Ivan Diatliuk <dyatlyukk.ivan@gmail.com>
2023-09-18 21:18:56 +0300

    added cartItem and ShoppingCart entities and implemented yaml change sets for creating appropriate tables for them

---
+++ src/main/java/org/diatliuk/bookstore/model/ShoppingCart.java
@@ -0,0 +1,29 @@
+package org.diatliuk.bookstore.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "shopping_carts")
+public class ShoppingCart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "shoppingCart")
+    private Set<CartItem> cartItems;
+}
