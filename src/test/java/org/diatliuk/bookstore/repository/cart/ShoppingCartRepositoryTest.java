cfde7e86606765a0651c1e859bcf4bbcffefda2c
Ivan Diatliuk <dyatlyukk.ivan@gmail.com>
2023-10-20 14:52:55 +0300

    added a shoppingCart repository test

---
+++ src/test/java/org/diatliuk/bookstore/repository/cart/ShoppingCartRepositoryTest.java
@@ -0,0 +1,44 @@
+package org.diatliuk.bookstore.repository.cart;
+
+import org.diatliuk.bookstore.model.ShoppingCart;
+import org.diatliuk.bookstore.model.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Locale;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class ShoppingCartRepositoryTest {
+    private static final String ADD_DEFAULT_USERS_AND_CARTS = "classpath:"
+            + "database/queries/carts/add-default-users-and-carts.sql";
+    private static final String DELETE_DEFAULT_USERS_AND_CARTS = "classpath:"
+            + "database/queries/carts/delete-default-users-and-carts.sql";
+    private static final Long TEST_VALID_USER_ID = 4L;
+
+    @Autowired
+    private ShoppingCartRepository cartRepository;
+
+    @Test
+    @DisplayName("Verify the getShoppingCartByUserId() method by using an existing id")
+    @Sql(
+            scripts = ADD_DEFAULT_USERS_AND_CARTS,
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
+    )
+    @Sql(
+            scripts = DELETE_DEFAULT_USERS_AND_CARTS,
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD
+    )
+    void getShoppingCartByUserId_withValidUserId() {
+        ShoppingCart actualCart = cartRepository.getShoppingCartByUserId(TEST_VALID_USER_ID);
+
+        assertNotNull(actualCart.getUser());
+        assertNotNull(actualCart.getCartItems());
+    }
+}
---
+++ src/test/resources/database/queries/carts/delete-default-users-and-carts.sql
@@ -0,0 +1,7 @@
+DELETE FROM users WHERE users.email = 'test@gnail.com';
+
+DELETE FROM cart_items WHERE cart_items.shopping_cart_id = 1;
+
+DELETE FROM books WHERE books.title = 'Test';
+
+DELETE FROM shopping_carts WHERE shopping_carts.id = 1;
