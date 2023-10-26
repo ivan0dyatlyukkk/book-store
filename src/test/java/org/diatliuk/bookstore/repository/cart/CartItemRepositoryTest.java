bc932a316c362549e525ca36bae2210fff15128b
Ivan Diatliuk <dyatlyukk.ivan@gmail.com>
2023-10-26 21:06:53 +0300

    improved the solution

--- src/test/java/org/diatliuk/bookstore/repository/cart/CartItemRepositoryTest.java
+++ src/test/java/org/diatliuk/bookstore/repository/cart/CartItemRepositoryTest.java
@@ -1,14 +1,20 @@
 package org.diatliuk.bookstore.repository.cart;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.math.BigDecimal;
+import org.diatliuk.bookstore.model.Book;
 import org.diatliuk.bookstore.model.CartItem;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
+import org.testcontainers.shaded.org.apache.commons.lang3.builder.EqualsBuilder;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -17,11 +23,25 @@ class CartItemRepositoryTest {
             + "database/queries/carts/add-default-users-and-carts.sql";
     private static final String DELETE_DEFAULT_USERS_AND_CARTS = "classpath:"
             + "database/queries/carts/delete-default-users-and-carts.sql";
+    private static final Book DEFAULT_BOOK_OF_CART_ITEM = new Book();
+
     private static final Long TEST_VALID_CART_ITEM_ID = 1L;
     private static final Long TEST_VALID_CART_ID = 1L;
 
     @Autowired
     private CartItemRepository cartItemRepository;
+
+    @BeforeAll
+    static void beforeAll() {
+        DEFAULT_BOOK_OF_CART_ITEM.setId(4L);
+        DEFAULT_BOOK_OF_CART_ITEM.setTitle("Test");
+        DEFAULT_BOOK_OF_CART_ITEM.setAuthor("ADMIN ADMIN");
+        DEFAULT_BOOK_OF_CART_ITEM.setIsbn("978-161-729-045-9");
+        DEFAULT_BOOK_OF_CART_ITEM.setPrice(BigDecimal.valueOf(200));
+        DEFAULT_BOOK_OF_CART_ITEM.setDescription("This is a description");
+        DEFAULT_BOOK_OF_CART_ITEM.setCoverImage("link.ua//images/1");
+        DEFAULT_BOOK_OF_CART_ITEM.setDeleted(false);
+    }
 
     @Test
     @DisplayName("Verify the findById() method by using an existing id")
@@ -55,5 +75,8 @@ class CartItemRepositoryTest {
 
         assertNotNull(actualCartItem);
         assertNotNull(actualCartItem.getShoppingCart());
+        assertEquals(TEST_VALID_CART_ID, actualCartItem.getShoppingCart().getId());
+        assertTrue(EqualsBuilder.reflectionEquals(DEFAULT_BOOK_OF_CART_ITEM,
+                                                  actualCartItem.getBook()));
     }
 }
